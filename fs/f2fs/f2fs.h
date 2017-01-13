01fd72e1b311780cf188ae0f4c1c94a5b167f6e5
Jaegeuk Kim <jaegeuk@kernel.org>
2017-01-13 14:54:32 -0800

    f2fs: add migrate_page to avoid patch conflicts
    This is disabled by default.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2389,6 +2389,10 @@ void f2fs_set_page_dirty_nobuffers(struct page *page);
 void f2fs_invalidate_page(struct page *page, unsigned int offset,
 			unsigned int length);
 int f2fs_release_page(struct page *page, gfp_t wait);
+#ifdef CONFIG_MIGRATION
+int f2fs_migrate_page(struct address_space *mapping, struct page *newpage,
+			struct page *page, enum migrate_mode mode);
+#endif
 
 /*
  * gc.c
